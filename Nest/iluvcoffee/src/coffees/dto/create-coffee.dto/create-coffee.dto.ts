5a3e16f5424a8ea77a953a862a3ab691f95a23b1
Yqc <1243188872@qq.com>
2024-07-16 14:10:05 +0800

    nestjs

--- Nest/iluvcoffee/src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ Nest/iluvcoffee/src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,5 +1,9 @@
+import { IsString } from 'class-validator';
 export class CreateCoffeeDto {
+  @IsString()  // 必填
   readonly name: string;
+  @IsString()
   readonly brand: string;
+  @IsString({ each: true }) // 必填 (each代表 string 数组)
   readonly flavors: string[];
 }
