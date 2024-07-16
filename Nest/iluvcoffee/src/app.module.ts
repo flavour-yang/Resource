52c3ce0fc420c89970667599e357fc2c7c801516
Yqc <1243188872@qq.com>
2024-07-16 17:34:48 +0800

    postgres

--- Nest/iluvcoffee/src/app.module.ts
+++ Nest/iluvcoffee/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'postgres',
+      autoLoadEntities: true, // 自动加载模块
+      synchronize: true, // 是否同步, 生产中禁用(会根据entity 自动生成数据库表)
+    }),
+  ],
   controllers: [AppController, CoffeesController],
   providers: [AppService, CoffeesService],
 })
